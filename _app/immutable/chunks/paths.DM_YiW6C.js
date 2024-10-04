ae24f43b5519e97cc3dea306670093301a879575
lajjo <lajjo1992@gmail.com>
2024-10-04 09:10:54 +0200

    Updates

--- _app/immutable/chunks/paths.DM_YiW6C.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_yer6qj)==null?void 0:s.base)??"/ap-browser-games";var e;const t=((e=globalThis.__sveltekit_yer6qj)==null?void 0:e.assets)??a;export{t as a,a as b};
