6d3809daef0ae84cc4d34cf5e2d6c82b9fb5b10e
lajjo <lajjo1992@gmail.com>
2024-10-05 19:21:21 +0200

    Updates

---
+++ _app/immutable/chunks/paths.CM_yqfag.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_6wywa8)==null?void 0:s.base)??"/ap-browser-games";var a;const t=((a=globalThis.__sveltekit_6wywa8)==null?void 0:a.assets)??e;export{t as a,e as b};
