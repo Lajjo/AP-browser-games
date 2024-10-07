8c652f30527b970f55c2939b55faee6ba5bcc00e
lajjo <lajjo1992@gmail.com>
2024-10-08 01:46:55 +0200

    Updates

---
+++ _app/immutable/chunks/paths.CPnlDDNf.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1q59xtd)==null?void 0:s.base)??"/ap-browser-games";var a;const t=((a=globalThis.__sveltekit_1q59xtd)==null?void 0:a.assets)??e;export{t as a,e as b};
